a6c3cde5abfb94ac0c4dee2f50f3735bbd1a938e
David-Zagal <david.andres9608@gmail.com>
2024-03-22 12:35:12 +0100

    Docs updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { isAuthenticatedGuard, isNotAuthenticatedGuard } from './auth/guards';
 
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot (routes, { useHash: false, preloadingStrategy: PreloadAllModules })],
+	imports: [RouterModule.forRoot (routes, { useHash: true })],
 	exports: [RouterModule]
 })
 export class AppRoutingModule {}
